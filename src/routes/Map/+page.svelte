7fcbb5b26730987c6d4265f49db9332a8658e99e
Md. Sakil Islam <sakilislam1080@gmail.com>
2023-09-02 22:05:50 +0600

    Map page create and all functionality added

---
+++ src/routes/Map/+page.svelte
@@ -0,0 +1,52 @@
+<script>
+  import { onMount } from "svelte";
+  import { Map, View } from "ol";
+  import { Tile as TileLayer, Vector as VectorLayer } from "ol/layer";
+  import { OSM } from "ol/source";
+  import GeoJSON from "ol/format/GeoJSON";
+  import { Style, Fill } from "ol/style";
+  import { fromLonLat } from "ol/proj";
+
+  let map;
+
+  onMount(() => {
+    // Create a map with full-screen width and height
+    map = new Map({
+      target: "map",
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      view: new View({
+        center: fromLonLat([0, 0]),
+        zoom: 5,
+      }),
+    });
+
+    // Load GeoJSON data and add it to the map
+    const geoJsonLayer = new VectorLayer({
+      source: new VectorLayer({
+        url:
+          "./countries.geojson",
+        format: new GeoJSON(),
+      }),
+      style: new Style({
+        fill: new Fill({
+          color: "#006a4e",
+        }),
+      }),
+      opacity: 0.75,
+    });
+
+    map.addLayer(geoJsonLayer);
+  });
+</script>
+
+<div id="map" style="width: 100%; height: 100vh;"></div>
+
+<style>
+  #map {
+    position: absolute;
+  }
+</style>
