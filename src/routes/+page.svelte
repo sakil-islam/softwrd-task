0e52be370576f408655d000f64861f3d169e1ce6
Md. Sakil Islam <sakilislam1080@gmail.com>
2023-09-03 12:07:54 +0600

    Add Navigation bar

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+</script>
+
+<main>
+  <nav class="bg-blue-500 p-4">
+    <div class="container mx-auto">
+      <ul class="flex space-x-4 items-center justify-center">
+        <li><a href="/" class="text-white text-[18px]">Home</a></li>
+        <li>
+          <a href="/Layout" class="text-white text-[18px]">Layout Page</a>
+        </li>
+        <li><a href="/Chart" class="text-white text-[18px]">Chart Page</a></li>
+        <li><a href="/Map" class="text-white text-[18px]">Map Page</a></li>
+      </ul>
+    </div>
+  </nav>
+</main>
+
+<style>
+</style>
